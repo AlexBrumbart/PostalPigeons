0c7d636b8badd4e292156d91f58d39ce4bce546d
AlexBrumbart <alex-bierl@gmx.de>
2022-07-25 17:37:12 +0200

    Added pigeon render into Pigeon Coop screen.

--- src/main/java/de/alexbrumbart/postalpigeons/rendering/PigeonCoopScreen.java
+++ src/main/java/de/alexbrumbart/postalpigeons/rendering/PigeonCoopScreen.java
@@ -3,8 +3,11 @@ package de.alexbrumbart.postalpigeons.rendering;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
+import de.alexbrumbart.postalpigeons.ModRegistries;
 import de.alexbrumbart.postalpigeons.PostalPigeons;
 import de.alexbrumbart.postalpigeons.blocks.PigeonCoopContainer;
+import de.alexbrumbart.postalpigeons.entity.Pigeon;
 import de.alexbrumbart.postalpigeons.util.NetworkHandler;
 import de.alexbrumbart.postalpigeons.util.packets.SBPigeonGoalPacket;
 import net.minecraft.client.Minecraft;
@@ -12,15 +15,20 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+
+import java.util.Objects;
 
 public class PigeonCoopScreen extends AbstractContainerScreen<PigeonCoopContainer> {
     private static final ResourceLocation texture = new ResourceLocation(PostalPigeons.ID, "textures/gui/pigeon_coop.png");
     private static final Component sendPigeonComponent = Component.translatable("postalpigeons.container.pigeon_coop.send");
 
     private EditBox editBox;
+    private Pigeon pigeon;
 
     public PigeonCoopScreen(PigeonCoopContainer menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -44,6 +52,8 @@ public class PigeonCoopScreen extends AbstractContainerScreen<PigeonCoopContaine
         renderTooltip(poseStack, mouseX, mouseY);
     }
 
+
+
     @Override
     protected void renderBg(PoseStack poseStack, float partialTick, int mouseX, int mouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
@@ -51,6 +61,26 @@ public class PigeonCoopScreen extends AbstractContainerScreen<PigeonCoopContaine
         RenderSystem.setShaderTexture(0, texture);
 
         blit(poseStack, leftPos, topPos, 0, 0, imageWidth, imageHeight);
+
+        if (pigeon == null && minecraft != null)
+            pigeon = ModRegistries.PIGEON.get().create(Objects.requireNonNull(minecraft.level));
+
+        if (pigeon != null) {
+            poseStack.pushPose();
+            poseStack.translate(leftPos + 31D, topPos + 67D, 0);
+            poseStack.scale(60, 60, 60);
+            poseStack.mulPose(Vector3f.ZP.rotationDegrees(180));
+            poseStack.mulPose(Vector3f.YP.rotationDegrees(30));
+
+            EntityRenderDispatcher erd = Minecraft.getInstance().getEntityRenderDispatcher();
+            MultiBufferSource.BufferSource immediate = Minecraft.getInstance().renderBuffers().bufferSource();
+            erd.setRenderShadow(false);
+            erd.render(pigeon, 0, 0, 0, 0, 1, poseStack, immediate, 0xF000F0);
+            erd.setRenderShadow(true);
+            immediate.endBatch();
+
+            poseStack.popPose();
+        }
     }
 
     @Override
