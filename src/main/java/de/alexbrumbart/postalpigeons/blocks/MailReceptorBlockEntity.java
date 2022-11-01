950749f81be2f26070490146f95686f5359fd5d4
AlexBrumbart <alex-bierl@gmx.de>
2022-11-01 13:29:22 +0100

    Added support for vanilla hoppers on the mail receptor.

--- src/main/java/de/alexbrumbart/postalpigeons/blocks/MailReceptorBlockEntity.java
+++ src/main/java/de/alexbrumbart/postalpigeons/blocks/MailReceptorBlockEntity.java
@@ -1,11 +1,14 @@
 package de.alexbrumbart.postalpigeons.blocks;
 
+import com.google.common.base.Suppliers;
 import de.alexbrumbart.postalpigeons.ModRegistries;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.Containers;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -18,8 +21,11 @@ import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
-public class MailReceptorBlockEntity extends BlockEntity implements MenuProvider {
+public class MailReceptorBlockEntity extends BlockEntity implements MenuProvider, WorldlyContainer {
+    private final Supplier<int[]> slots = Suppliers.memoize(() -> IntStream.range(0, 18).toArray());
     private final ItemStackHandler inventory = new ItemStackHandler(18) {
 
         @Override
@@ -84,5 +90,69 @@ public class MailReceptorBlockEntity extends BlockEntity implements MenuProvider
 
         inventory.deserializeNBT(tag.getCompound("inventory"));
         name = tag.getString("name");
+    }
+
+    // Container implementation for hopper integration
+
+    @Override
+    public int[] getSlotsForFace(Direction side) {
+        return slots.get();
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int index, ItemStack itemStack, @Nullable Direction direction) {
+        return true;
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int pIndex, ItemStack pStack, Direction pDirection) {
+        return true;
+    }
+
+    @Override
+    public int getContainerSize() {
+        return inventory.getSlots();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (int i = 0; i < inventory.getSlots(); i++) {
+            if (!inventory.getStackInSlot(i).isEmpty())
+                return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public ItemStack getItem(int slot) {
+        return inventory.getStackInSlot(slot);
+    }
+
+    @Override
+    public ItemStack removeItem(int slot, int amount) {
+        return inventory.extractItem(slot, amount, false);
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int slot) {
+        return inventory.extractItem(slot, 64, false);
+    }
+
+    @Override
+    public void setItem(int slot, ItemStack stack) {
+        inventory.setStackInSlot(slot, stack);
+    }
+
+    @Override
+    public boolean stillValid(Player pPlayer) {
+        return true;
+    }
+
+    @Override
+    public void clearContent() {
+        for (int i = 0; i < inventory.getSlots(); i++) {
+            inventory.setStackInSlot(i, ItemStack.EMPTY);
+        }
     }
 }
