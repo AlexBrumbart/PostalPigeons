c74a4fb9610c3328757d2903465c5556b4083825
AlexBrumbart <alex-bierl@gmx.de>
2022-11-18 21:17:51 +0100

    Added a Game Test to check the capability on the mail receptor.

---
+++ src/main/java/de/alexbrumbart/postalpigeons/test/GameTests.java
@@ -0,0 +1,31 @@
+package de.alexbrumbart.postalpigeons.test;
+
+import de.alexbrumbart.postalpigeons.PostalPigeons;
+import net.minecraft.core.BlockPos;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestAssertException;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.gametest.GameTestHolder;
+import net.minecraftforge.gametest.PrefixGameTestTemplate;
+import net.minecraftforge.items.IItemHandler;
+
+@GameTestHolder(value = PostalPigeons.ID)
+public class GameTests {
+    private GameTests() {
+        throw new IllegalStateException("Utility class!");
+    }
+
+    @GameTest(template = "mail_receptor")
+    @PrefixGameTestTemplate(false)
+    public static void mailReceptorCap(GameTestHelper helper) {
+        helper.succeedIf(() -> {
+            BlockEntity receptor = helper.getBlockEntity(new BlockPos(1, 2, 1));
+            IItemHandler handler = receptor.getCapability(ForgeCapabilities.ITEM_HANDLER).orElseThrow(() -> new GameTestAssertException("handler null"));
+            if (handler.getSlots() != 18) {
+                throw new GameTestAssertException("handler has not correct size");
+            }
+        });
+    }
+}
